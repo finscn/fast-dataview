ac2abcefa2bcbd2c2ad2c90ecd36ffeed98886f2
Rocky Wei <finscn@gmail.com>
2017-03-15 07:09:26 +0800

    first commit

---
+++ src/FastDataView.js
@@ -0,0 +1,151 @@
+"use strict";
+
+(function(exports) {
+
+    var FastDataView = function(buffer, byteOffset, byteLength) {
+        this.buffer = buffer;
+        this.byteOffset = byteOffset || 0;
+        this.byteLength = byteLength || buffer.byteLength;
+
+        this.byteArray = new Uint8Array(buffer);
+        this.dataView = new DataView(buffer, byteOffset, byteLength);
+
+        this.initCacheArray();
+    };
+
+    FastDataView.prototype.initCacheArray = function() {
+        this.cacheBuffer = new ArrayBuffer(8);
+
+        this.uint8Array = new Uint8Array(this.cacheBuffer);
+        this.int8Array = new Int8Array(this.cacheBuffer);
+
+        this.uint16Array = new Uint16Array(this.cacheBuffer);
+        this.int16Array = new Int16Array(this.cacheBuffer);
+
+        this.uint32Array = new Uint32Array(this.cacheBuffer);
+        this.int32Array = new Int32Array(this.cacheBuffer);
+
+        this.float32Array = new Float32Array(this.cacheBuffer);
+        this.float64Array = new Float64Array(this.cacheBuffer);
+    };
+
+    FastDataView.prototype.setUint8 = function(offset, value) {
+        this.byteArray[offset] = value;
+    };
+
+    FastDataView.prototype.getUint8 = function(offset) {
+        return this.byteArray[offset];
+    };
+
+    FastDataView.prototype.setInt8 = function(offset, value) {
+        this.byteArray[offset] = value;
+    };
+
+    FastDataView.prototype.getInt8 = function(offset) {
+        this.uint8Array[0] = this.byteArray[offset];
+        return this.int8Array[0];
+    };
+
+    FastDataView.prototype.setUint16 = function(offset, value) {
+        this.byteArray[offset] = value >>> 8;
+        this.byteArray[offset + 1] = value;
+    };
+
+    FastDataView.prototype.getUint16 = function(offset) {
+        var a = this.byteArray[offset];
+        var b = this.byteArray[offset + 1];
+        return (a << 8) | b;
+    };
+
+    FastDataView.prototype.setInt16 = function(offset, value) {
+        this.byteArray[offset] = value >>> 8;
+        this.byteArray[offset + 1] = value;
+    };
+
+    FastDataView.prototype.getInt16 = function(offset) {
+        this.uint8Array[0] = this.byteArray[offset + 1];
+        this.uint8Array[1] = this.byteArray[offset];
+        return this.int16Array[0];
+    };
+
+    FastDataView.prototype.setUint32 = function(offset, value) {
+        // this.dataView.setUint32(offset, value);
+        this.byteArray[offset] = value >>> 24;
+        this.byteArray[offset + 1] = (value >>> 16);
+        this.byteArray[offset + 2] = (value >>> 8);
+        this.byteArray[offset + 3] = value;
+    };
+
+    FastDataView.prototype.getUint32 = function(offset) {
+        // var a = this.byteArray[offset];
+        // var b = this.byteArray[offset + 1];
+        // var c = this.byteArray[offset + 2];
+        // var d = this.byteArray[offset + 3];
+        // return (a << 24) | (b << 16) | (c << 8) | d;
+        return this.dataView.getUint32(offset);
+    };
+
+    FastDataView.prototype.setInt32 = function(offset, value) {
+        this.byteArray[offset] = value >>> 24;
+        this.byteArray[offset + 1] = (value >>> 16);
+        this.byteArray[offset + 2] = (value >>> 8);
+        this.byteArray[offset + 3] = value;
+    };
+
+    FastDataView.prototype.getInt32 = function(offset) {
+        this.uint8Array[0] = this.byteArray[offset + 3];
+        this.uint8Array[1] = this.byteArray[offset + 2];
+        this.uint8Array[2] = this.byteArray[offset + 1];
+        this.uint8Array[3] = this.byteArray[offset];
+        return this.int32Array[0];
+    };
+
+    FastDataView.prototype.setFloat32 = function(offset, value) {
+        this.float32Array[0] = value;
+        this.byteArray[offset] = this.uint8Array[3];
+        this.byteArray[offset + 1] = this.uint8Array[2];
+        this.byteArray[offset + 2] = this.uint8Array[1];
+        this.byteArray[offset + 3] = this.uint8Array[0];
+    };
+
+    FastDataView.prototype.getFloat32 = function(offset) {
+        this.uint8Array[0] = this.byteArray[offset + 3];
+        this.uint8Array[1] = this.byteArray[offset + 2];
+        this.uint8Array[2] = this.byteArray[offset + 1];
+        this.uint8Array[3] = this.byteArray[offset];
+        return this.float32Array[0];
+    };
+
+    FastDataView.prototype.setFloat64 = function(offset, value) {
+        this.float64Array[0] = value;
+        this.byteArray[offset] = this.uint8Array[7];
+        this.byteArray[offset + 1] = this.uint8Array[6];
+        this.byteArray[offset + 2] = this.uint8Array[5];
+        this.byteArray[offset + 3] = this.uint8Array[4];
+        this.byteArray[offset + 4] = this.uint8Array[3];
+        this.byteArray[offset + 5] = this.uint8Array[2];
+        this.byteArray[offset + 6] = this.uint8Array[1];
+        this.byteArray[offset + 7] = this.uint8Array[0];
+    };
+
+    FastDataView.prototype.getFloat64 = function(offset) {
+        this.uint8Array[0] = this.byteArray[offset + 7];
+        this.uint8Array[1] = this.byteArray[offset + 6];
+        this.uint8Array[2] = this.byteArray[offset + 5];
+        this.uint8Array[3] = this.byteArray[offset + 4];
+        this.uint8Array[4] = this.byteArray[offset + 3];
+        this.uint8Array[5] = this.byteArray[offset + 2];
+        this.uint8Array[6] = this.byteArray[offset + 1];
+        this.uint8Array[7] = this.byteArray[offset];
+        return this.float64Array[0];
+    };
+
+    if (exports) {
+        exports.FastDataView = FastDataView;
+    }
+
+    if (typeof module != "undefined") {
+        module.exports = FastDataView;
+    }
+
+})(typeof window !== "undefined" ? window : null);
---
+++ test/fastdataview-vs-dataview.js
@@ -0,0 +1,183 @@
+var FastDataView = require("../src/FastDataView");
+
+var bufferSize = 100 * 26;
+var testCount = 10000;
+
+
+function testWrite(fast) {
+    var buffer = new ArrayBuffer(bufferSize);
+    var DataViewClass = fast ? FastDataView : DataView;
+    var name = fast ? 'FastDataView' : 'DataView';
+
+    console.time(name + ' wirte: ');
+    for (var c = 0; c < testCount; c++) {
+        var view = new DataViewClass(buffer);
+        for (var i = 0; i < bufferSize;) {
+            view.setUint8(i, 255);
+            i += 1;
+            view.setInt8(i, 127);
+            i += 1;
+            view.setUint16(i, 65535);
+            i += 2;
+            view.setInt16(i, 32767);
+            i += 2;
+            view.setUint32(i, 4294967295);
+            i += 4;
+            view.setInt32(i, 2147483647);
+            i += 4;
+            view.setFloat32(i, 3.1415926);
+            i += 4;
+            view.setFloat64(i, Math.E);
+            i += 8;
+        }
+    }
+    console.timeEnd(name + ' wirte: ');
+}
+
+function testRead(fast) {
+    var buffer = new ArrayBuffer(bufferSize);
+    var arr = new Uint8Array(buffer);
+    for (var i = 0; i < arr.byteLength; i++) {
+        arr[i] = i + 1;
+    }
+
+    var DataViewClass = fast ? FastDataView : DataView;
+    var name = fast ? 'FastDataView' : 'DataView';
+
+    console.time(name + ' read: ');
+    for (var c = 0; c < testCount; c++) {
+        var view = new DataViewClass(buffer);
+        for (var i = 0; i < bufferSize;) {
+            view.getUint8(i);
+            i += 1;
+            view.getInt8(i);
+            i += 1;
+            view.getUint16(i);
+            i += 2;
+            view.getInt16(i);
+            i += 2;
+            view.getUint32(i);
+            i += 4;
+            view.getInt32(i);
+            i += 4;
+            view.getFloat32(i);
+            i += 4;
+            view.getFloat64(i);
+            i += 8;
+        }
+    }
+    console.timeEnd(name + ' read: ');
+}
+
+function verify() {
+    var size = 26;
+    var buffer1 = new ArrayBuffer(size);
+    var view = new DataView(buffer1);
+    for (var i = 0; i < size;) {
+        view.setUint8(i, 255);
+        i += 1;
+        view.setInt8(i, 127);
+        i += 1;
+        view.setUint16(i, 65535);
+        i += 2;
+        view.setInt16(i, 32767);
+        i += 2;
+        view.setUint32(i, 4294967295);
+        i += 4;
+        view.setInt32(i, 2147483647);
+        i += 4;
+        view.setFloat32(i, 3.1415926);
+        i += 4;
+        view.setFloat64(i, Math.E);
+        i += 8;
+    }
+
+    var buffer2 = new ArrayBuffer(size);
+    var view = new FastDataView(buffer2);
+    for (var i = 0; i < size;) {
+        view.setUint8(i, 255);
+        i += 1;
+        view.setInt8(i, 127);
+        i += 1;
+        view.setUint16(i, 65535);
+        i += 2;
+        view.setInt16(i, 32767);
+        i += 2;
+        view.setUint32(i, 4294967295);
+        i += 4;
+        view.setInt32(i, 2147483647);
+        i += 4;
+        view.setFloat32(i, 3.1415926);
+        i += 4;
+        view.setFloat64(i, Math.E);
+        i += 8;
+    }
+
+    var bytes1 = new Uint8Array(buffer1);
+    var bytes2 = new Uint8Array(buffer2);
+
+    for (var i = 0; i < size; i++) {
+        if (bytes1[i] !== bytes2[i]) {
+            console.log("wirte FAILED");
+            return;
+        }
+    }
+    console.log("wirte OK");
+
+
+    var view1 = new DataView(buffer1);
+    var view2 = new FastDataView(buffer2);
+
+    for (var i = 0; i < size;) {
+        if (view1.getUint8(i) !== view2.getUint8(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 1;
+        if (view1.getInt8(i) !== view2.getInt8(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 1;
+        if (view1.getUint16(i) !== view2.getUint16(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 2;
+        if (view1.getInt16(i) !== view2.getInt16(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 2;
+        if (view1.getUint32(i) !== view2.getUint32(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 4;
+        if (view1.getInt32(i) !== view2.getInt32(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 4;
+        if (view1.getFloat32(i) !== view2.getFloat32(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 4;
+        if (view1.getFloat64(i) !== view2.getFloat64(i)) {
+            console.log("read FAILED ", i);
+            return;
+        };
+        i += 8;
+    }
+
+    console.log("read OK");
+
+}
+
+
+testWrite();
+testRead();
+testWrite(true);
+testRead(true);
+verify();
